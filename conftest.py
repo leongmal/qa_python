0b1895bfdeebf30869d12f40768f6b5a9f3dbecf
leongmal <leonik1968@gmail.com>
2025-09-27 15:10:16 +0300

    The unnecessary fixture has been removed

--- conftest.py
+++ conftest.py
@@ -4,9 +4,4 @@ from main import BooksCollector
 @pytest.fixture
 def collector():
     return BooksCollector()
-@pytest.fixture
-def add_book_new(collector):
-    def _add_book(name, ganre=None):
-        collector.add_new_book(name)
-        collector.set_book_genre(name,ganre)
-    return _add_book
+
--- tests.py
+++ tests.py
@@ -36,15 +36,18 @@ class TestBooksCollector:
         collector.set_book_genre('Солярис', 'Фантастика')
         assert 'Фантастика' not in collector.books_genre
 
-    def test_get_book_genre_valid(self, collector, add_book_new):
-        add_book_new('Туманность Андромеды', 'Фантастика')
+    def test_get_book_genre_valid(self, collector):
+        collector.add_new_book('Туманность Андромеды')
+        collector.set_book_genre('Туманность Андромеды', 'Фантастика')
         assert collector.get_book_genre('Туманность Андромеды') == 'Фантастика'
 
-    def test_get_books_with_specific_genre_valid(self, collector, add_book_new):
-        add_book_new('Челюсти', 'Ужасы')
-        add_book_new('Киндза-дза', 'Комедии')
-        add_book_new('Туманность Андромеды', 'Фантастика')
-
+    def test_get_books_with_specific_genre_valid(self, collector):
+        collector.add_new_book('Челюсти')
+        collector.add_new_book('Киндза-дза')
+        collector.add_new_book('Туманность Андромеды')
+        collector.set_book_genre('Челюсти', 'Ужасы')
+        collector.set_book_genre('Киндза-дза', 'Комедии')
+        collector.set_book_genre('Туманность Андромеды', 'Фантастика')
         result = collector.get_books_with_specific_genre('Комедии')
         assert result == ['Киндза-дза']
 
@@ -52,17 +55,22 @@ class TestBooksCollector:
         result = collector.get_books_with_specific_genre('Фэнтези')
         assert result == []
 
-    def test_get_books_for_children_valid(self, collector, add_book_new):
-        add_book_new('Незнайка', 'Мультфильмы')
-        add_book_new('Буратино', 'Комедии')
-        add_book_new('Челюсти', 'Ужасы')
+    def test_get_books_for_children_valid(self, collector):
+        collector.add_new_book('Незнайка')
+        collector.add_new_book('Буратино')
+        collector.add_new_book('Челюсти')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+        collector.set_book_genre('Буратино', 'Комедии')
+        collector.set_book_genre('Челюсти', 'Ужасы')
 
         result = collector.get_books_for_children()
         assert result == ['Незнайка', 'Буратино']
 
-    def test_get_books_for_children_all_restricted(self, collector, add_book_new):
-        add_book_new('Челюсти', 'Ужасы')
-        add_book_new('Игла', 'Детективы')
+    def test_get_books_for_children_all_restricted(self, collector):
+        collector.add_new_book('Челюсти',)
+        collector.add_new_book('Игла')
+        collector.set_book_genre('Челюсти', 'Ужасы')
+        collector.set_book_genre('Игла', 'Детективы')
 
         result = collector.get_books_for_children()
         assert result == []
